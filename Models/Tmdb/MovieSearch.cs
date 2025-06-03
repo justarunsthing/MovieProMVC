ca29d761c516ced8f2adb82d9e324ac5d0785922
justarunsthing <justarunsthing@outlook.com>
2025-06-03 09:21:34 +0100

    Added MovieSearch class

---
+++ Models/Tmdb/MovieSearch.cs
@@ -0,0 +1,29 @@
+﻿namespace MovieProMVC.Models.Tmdb
+{
+
+    public class MovieSearch
+    {
+        public int Page { get; set; }
+        public MovieSearchResult[] Results { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalResults { get; set; }
+    }
+
+    public class MovieSearchResult
+    {
+        public bool Adult { get; set; }
+        public string BackdropPath { get; set; }
+        public int[] GenreIds { get; set; }
+        public int Id { get; set; }
+        public string OriginalLanguage { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Overview { get; set; }
+        public float Popularity { get; set; }
+        public string PosterPath { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Title { get; set; }
+        public bool Video { get; set; }
+        public float VoteAverage { get; set; }
+        public int VoteCount { get; set; }
+    }
+}
