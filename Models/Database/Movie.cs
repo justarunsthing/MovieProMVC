93e1180c42b7123ec18aa9d752e8a276e35b2a84
justarunsthing <justarunsthing@outlook.com>
2025-06-04 09:24:38 +0100

    Added Movie class & MovieRating enum

---
+++ Models/Database/Movie.cs
@@ -0,0 +1,35 @@
+﻿using MovieProMVC.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieProMVC.Models.Database
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string TagLine { get; set; }
+        public string Overview { get; set; }
+        public int RunTime { get; set; }
+
+        [DataType(DataType.Date)] // Only get date portion, not time. Time will be stored as 0
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        public MovieRating Rating { get; set; }
+        public float VoteAverage { get; set; }
+        public byte[] Poster { get; set; }
+        public string PosterType { get; set; }
+        public byte[] Backdrop { get; set; }
+        public string BackdropType { get; set; }
+        public string TrailerUrl { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Poster Image")]
+        public IFormFile PosterFile { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Backdrop Image")]
+        public IFormFile BackdropFile { get; set; }
+    }
+}
