a6270eb64bedf3999912964a4c13cea16a46b133
justarunsthing <justarunsthing@outlook.com>
2025-06-05 09:07:00 +0100

    Added MovieCollection class

---
+++ Models/Database/MovieCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieProMVC.Models.Database
+{
+    public class MovieCollection
+    {
+        public int Id { get; set; }
+        public int CollectionId { get; set; }
+        public int MovieId { get; set; }
+        public int Order { get; set; }
+
+        // Store entire record referenced by the FKs
+        public Collection Collection { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
