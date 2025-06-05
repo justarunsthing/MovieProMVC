829cf2f69d6d2830b25d4df3c1dc69179afe33ae
justarunsthing <justarunsthing@outlook.com>
2025-06-05 09:13:32 +0100

    Added MovieCrew model

---
+++ Models/Database/MovieCrew.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieProMVC.Models.Database
+{
+    public class MovieCrew
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int CrewId { get; set; }
+        public string Department { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public string ImageUrl { get; set; }
+
+        // Store entire record referenced by the FK
+        public Movie Movie { get; set; }
+    }
+}
