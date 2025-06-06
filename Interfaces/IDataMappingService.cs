1ee0fd33e0f98a2f896658016f38aa9ecdee8d8e
justarunsthing <justarunsthing@outlook.com>
2025-06-06 18:19:17 +0100

    Added IDataMappingService interface

---
+++ Interfaces/IDataMappingService.cs
@@ -0,0 +1,11 @@
+﻿using MovieProMVC.Models.Database;
+using MovieProMVC.Models.Tmdb;
+
+namespace MovieProMVC.Interfaces
+{
+    public interface IDataMappingService
+    {
+        Task<Movie> MapMovieDetailsAsync(MovieDetails movie);
+        ActorDetails MapActorDetails(ActorDetails actor);
+    }
+}
