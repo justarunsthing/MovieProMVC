3022f13080286be9d113b494c182ea13dfaba5c7
justarunsthing <justarunsthing@outlook.com>
2025-06-06 18:09:42 +0100

    Added LandingPageViewModel

---
+++ Models/ViewModels/LandingPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MovieProMVC.Models.Database;
+using MovieProMVC.Models.Tmdb;
+
+namespace MovieProMVC.Models.ViewModels
+{
+    public class LandingPageViewModel
+    {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
+    }
+}
