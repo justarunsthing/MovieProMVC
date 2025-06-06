f40abb2b5b5c93c09263b5a898ff0330a91dc784
justarunsthing <justarunsthing@outlook.com>
2025-06-06 18:21:15 +0100

    Added IImageService interface

---
+++ Interfaces/IImageService.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieProMVC.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile poster);
+        Task<byte[]> EncodeImageUrlAsync(string imageUrl);
+        string DecodeImage(byte[] poster, string contentType);
+    }
+}
