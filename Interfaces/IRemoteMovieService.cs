82a3e3903da1c08487be023b53d4666cc4d6ddd0
justarunsthing <justarunsthing@outlook.com>
2025-06-06 18:16:29 +0100

    Added IRemoteMovieService interface

---
+++ Interfaces/IRemoteMovieService.cs
@@ -0,0 +1,12 @@
+﻿using MovieProMVC.Enums;
+using MovieProMVC.Models.Tmdb;
+
+namespace MovieProMVC.Interfaces
+{
+    public interface IRemoteMovieService
+    {
+        Task<MovieDetails> GetMovieDetailsAsync(int id);
+        Task<MovieSearch> SearchMoviesAsync(MovieCategory category, int count);
+        Task<ActorDetails> GetActorDetailsAsync(int id);
+    }
+}
