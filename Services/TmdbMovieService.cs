0864162070f4fa17ed39dbea83021d6d533255ee
justarunsthing <justarunsthing@outlook.com>
2025-06-09 16:57:46 +0100

    Added dependencies to TmdbMovieService

--- Services/TmdbMovieService.cs
+++ Services/TmdbMovieService.cs
@@ -1,11 +1,22 @@
 ﻿using MovieProMVC.Enums;
 using MovieProMVC.Interfaces;
 using MovieProMVC.Models.Tmdb;
+using MovieProMVC.Models.Settings;
+using Microsoft.Extensions.Options;
 
 namespace MovieProMVC.Services
 {
     public class TmdbMovieService : IRemoteMovieService
     {
+        private readonly AppSettings _appSettings;
+        private readonly IHttpClientFactory _httpClient;
+
+        public TmdbMovieService(IOptions<AppSettings> appSettings, IHttpClientFactory httpClient)
+        {
+            _appSettings = appSettings.Value;
+            _httpClient = httpClient;
+        }
+
         public Task<ActorDetails> GetActorDetailsAsync(int id)
         {
             throw new NotImplementedException();
