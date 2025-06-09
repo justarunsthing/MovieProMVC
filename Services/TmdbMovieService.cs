78f1ede56492eeb50a1b988c22e6f9d9b79b6adb
justarunsthing <justarunsthing@outlook.com>
2025-06-09 16:54:21 +0100

    Added TmdbMovieService class

---
+++ Services/TmdbMovieService.cs
@@ -0,0 +1,24 @@
+﻿using MovieProMVC.Enums;
+using MovieProMVC.Interfaces;
+using MovieProMVC.Models.Tmdb;
+
+namespace MovieProMVC.Services
+{
+    public class TmdbMovieService : IRemoteMovieService
+    {
+        public Task<ActorDetails> GetActorDetailsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MovieDetails> GetMovieDetailsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MovieSearch> SearchMoviesAsync(MovieCategory category, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
