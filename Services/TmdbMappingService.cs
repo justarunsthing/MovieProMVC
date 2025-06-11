d7cd13b99b26bf1a0e96f18529bef5c4f5817f2e
justarunsthing <justarunsthing@outlook.com>
2025-06-11 19:59:18 +0100

    Added TmdbMappingService class

---
+++ Services/TmdbMappingService.cs
@@ -0,0 +1,30 @@
+﻿using MovieProMVC.Interfaces;
+using MovieProMVC.Models.Tmdb;
+using MovieProMVC.Models.Database;
+using MovieProMVC.Models.Settings;
+using Microsoft.Extensions.Options;
+
+namespace MovieProMVC.Services
+{
+    public class TmdbMappingService : IDataMappingService
+    {
+        private readonly AppSettings _appSettings;
+        private readonly IImageService _imageService;
+
+        public TmdbMappingService(IOptions<AppSettings> appSettings, IImageService imageService)
+        {
+            _appSettings = appSettings.Value;
+            _imageService = imageService;
+        }
+
+        public ActorDetails MapActorDetails(ActorDetails actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> MapMovieDetailsAsync(MovieDetails movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
