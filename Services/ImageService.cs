5cd1c857ba7cd4be5845961c8099c5527f181d02
justarunsthing <justarunsthing@outlook.com>
2025-06-12 08:50:56 +0100

    Added ImageService class

---
+++ Services/ImageService.cs
@@ -0,0 +1,22 @@
+﻿using MovieProMVC.Interfaces;
+
+namespace MovieProMVC.Services
+{
+    public class ImageService : IImageService
+    {
+        public string DecodeImage(byte[] poster, string contentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> EncodeImageAsync(IFormFile poster)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> EncodeImageUrlAsync(string imageUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
