9aceb91f80489ad0fb64939beb885763045080ac
justarunsthing <justarunsthing@outlook.com>
2025-06-13 08:52:17 +0100

    Inject dependencies

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MovieProMVC.Data;
+using MovieProMVC.Interfaces;
 using MovieProMVC.Models;
 
 namespace MovieProMVC.Controllers
@@ -7,10 +9,14 @@ namespace MovieProMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IRemoteMovieService _tmdbMovieService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IRemoteMovieService tmdbMovieService)
         {
             _logger = logger;
+            _context = context;
+            _tmdbMovieService = tmdbMovieService;
         }
 
         public IActionResult Index()
