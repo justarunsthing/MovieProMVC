0401f3b6a9830c7d00497eb6444b7f7d6653f417
justarunsthing <justarunsthing@outlook.com>
2025-06-13 09:07:23 +0100

    Added MoviesController & dependencies

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using MovieProMVC.Data;
+using MovieProMVC.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using MovieProMVC.Models.Settings;
+
+namespace MovieProMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppSettings _appSettings;
+        private readonly ApplicationDbContext _context;
+        private readonly IImageService _imageService;
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _tmdbMappingService;
+
+        public MoviesController(AppSettings appSettings, ApplicationDbContext context, IImageService imageService, IRemoteMovieService tmdbMovieService, IDataMappingService tmdbMappingService)
+        {
+            _appSettings = appSettings;
+            _context = context;
+            _imageService = imageService;
+            _tmdbMovieService = tmdbMovieService;
+            _tmdbMappingService = tmdbMappingService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
