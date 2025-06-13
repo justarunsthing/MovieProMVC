485d9d5e5ea97198bbc3136b8cb4d944610b315d
justarunsthing <justarunsthing@outlook.com>
2025-06-13 08:58:03 +0100

    Populate Index view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MovieProMVC.Data;
+using MovieProMVC.Enums;
 using MovieProMVC.Interfaces;
 using MovieProMVC.Models;
+using MovieProMVC.Models.ViewModels;
 
 namespace MovieProMVC.Controllers
 {
@@ -19,9 +23,22 @@ namespace MovieProMVC.Controllers
             _tmdbMovieService = tmdbMovieService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            const int count = 16;
+            var model = new LandingPageViewModel
+            {
+                CustomCollections = await _context.Collection
+                                            .Include(c => c.MovieCollections)
+                                            .ThenInclude(mc => mc.Movie)
+                                            .ToListAsync(),
+                NowPlaying = await _tmdbMovieService.SearchMoviesAsync(MovieCategory.now_playing, count),
+                Popular = await _tmdbMovieService.SearchMoviesAsync(MovieCategory.popular, count),
+                TopRated = await _tmdbMovieService.SearchMoviesAsync(MovieCategory.top_rated, count),
+                Upcoming = await _tmdbMovieService.SearchMoviesAsync(MovieCategory.upcoming, count)
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
