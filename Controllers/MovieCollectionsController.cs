54016f020e699f7bf2c73ecf26033dcb1e18ff78
justarunsthing <justarunsthing@outlook.com>
2025-06-17 08:55:44 +0100

    Inject ApplicationDbContext dependency

--- Controllers/MovieCollectionsController.cs
+++ Controllers/MovieCollectionsController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieProMVC.Data;
 
 namespace MovieProMVC.Controllers
 {
     public class MovieCollectionsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public MovieCollectionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
