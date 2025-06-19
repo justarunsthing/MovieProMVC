92d854e63f1e973ddc51a8c541acd40c0c8610e8
justarunsthing <justarunsthing@outlook.com>
2025-06-19 08:47:27 +0100

    Inject dependencies

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieProMVC.Interfaces;
 
 namespace MovieProMVC.Controllers
 {
     public class ActorsController : Controller
     {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _mappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService mappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _mappingService = mappingService;
+        }
+
         public IActionResult Index()
         {
             return View();
