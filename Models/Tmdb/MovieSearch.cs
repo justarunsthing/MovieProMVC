e72fba88956994ba8b059ed1b12fbde6e3a97e9f
justarunsthing <justarunsthing@outlook.com>
2025-06-21 21:44:37 +0100

    Updated MovieSearch properties

--- Models/Tmdb/MovieSearch.cs
+++ Models/Tmdb/MovieSearch.cs
@@ -11,19 +11,19 @@
 
     public class MovieSearchResult
     {
-        public bool Adult { get; set; }
-        public string BackdropPath { get; set; }
-        public int[] GenreIds { get; set; }
-        public int Id { get; set; }
-        public string OriginalLanguage { get; set; }
-        public string OriginalTitle { get; set; }
-        public string Overview { get; set; }
-        public float Popularity { get; set; }
-        public string PosterPath { get; set; }
-        public string ReleaseDate { get; set; }
-        public string Title { get; set; }
-        public bool Video { get; set; }
-        public float VoteAverage { get; set; }
-        public int VoteCount { get; set; }
+        public bool adult { get; set; }
+        public string backdrop_path { get; set; }
+        public int[] genre_ids { get; set; } = [];
+        public int id { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public string overview { get; set; }
+        public float popularity { get; set; }
+        public string poster_path { get; set; }
+        public string release_date { get; set; }
+        public string title { get; set; }
+        public bool video { get; set; }
+        public float vote_average { get; set; }
+        public int vote_count { get; set; }
     }
 }
